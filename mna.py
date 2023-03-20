70252e858b444b615d0954ae8d8f561fa1c21284
Ólafur Bjarki Bogason <olafur@genkiinstruments.com>
2023-03-20 17:50:40 +0000

    Added a bunch of tests, sympy and implemented new component stamps. CCCS is currently underway

--- mna.py
+++ mna.py
@@ -1,12 +1,15 @@
 import numpy as np
 
 
-# The unknown current flows into nodes 1/3 (+) and out of nodes 0/2 (-)
+# Assumptions
+# -----------
+# - Nodes are numbered 0, 1, 2, ... (no gaps)
+# - The unknown current flows into nodes 1/3 (+) and out of nodes 0/2 (-)
+# n1 (+) --\   /-- (+) n3
+#           ? ?
+# n0 (-) --/   \-- (-) n2
+
 class Component:
-    # + n1 --\
-    #         ?
-    #         |
-    # - n0 --/
     def __init__(self, n1, n0, value=0, name=None):
         self.n1, self.n0 = n1, n0
         self.name = name
@@ -19,6 +22,27 @@ class Component:
         return 0
 
 
+class ResistiveVoltageSource(Component):
+    def __init__(self, n1, n0, volt=0, resistance=0, name=None):
+        super().__init__(n1, n0, volt, name)
+        self.volt, self.resistance = volt, resistance
+
+    def num_unknown_currents(self):
+        return 1
+
+    def stamp(self, G, B, C, D, E, unknown_currents):
+        idx = num_unknown_currents(unknown_currents)
+        unknown_currents.append(self)
+
+        B[self.n1, idx] += 1
+        B[self.n0, idx] -= 1
+        C[idx, self.n1] += 1
+        C[idx, self.n0] -= 1
+        D[idx, idx] -= self.resistance
+
+        E[G.shape[0] + idx] = self.volt
+
+
 class VoltageSource(Component):
     def __init__(self, n1, n0, value=0, name=None):
         super().__init__(n1, n0, value, name)
@@ -26,8 +50,8 @@ class VoltageSource(Component):
     def num_unknown_currents(self):
         return 1
 
-    def stamp(self, G, B, C, _D, _I, E, unknown_currents):
-        idx = num_unknown_currents(_unknown_currents)
+    def stamp(self, G, B, C, _D, E, unknown_currents):
+        idx = num_unknown_currents(unknown_currents)
         unknown_currents.append(self)
 
         B[self.n1, idx] += 1
@@ -35,14 +59,14 @@ class VoltageSource(Component):
         C[idx, self.n1] += 1
         C[idx, self.n0] -= 1
 
-        E[len(G) + idx] = self.value
+        E[G.shape[0] + idx] = self.value
 
 
 class CurrentSource(Component):
     def __init__(self, n1, n0, value=0, name=None):
         super().__init__(n1, n0, value, name)
 
-    def stamp(self, _G, _B, _C, _D, _I, E, _unknown_currents):
+    def stamp(self, _G, _B, _C, _D, E, _unknown_currents):
         E[self.n1] += self.value
         E[self.n0] -= self.value
 
@@ -51,7 +75,7 @@ class Resistor(Component):
     def __init__(self, n1, n0, value=0, name=None):
         super().__init__(n1, n0, value, name)
 
-    def stamp(self, G, _B, _C, _D, _I, _E, _unknown_currents):
+    def stamp(self, G, _B, _C, _D, _E, _unknown_currents):
         G[self.n1, self.n1] += 1 / self.value
         G[self.n0, self.n0] += 1 / self.value
         G[self.n1, self.n0] -= 1 / self.value
@@ -62,7 +86,7 @@ class Capacitor(Component):
     def __init__(self, n1, n0, value=0, name=None):
         super().__init__(n1, n0, value, name)
 
-    def stamp(self, G, _B, _C, _D, _I, _E, _unknown_currents):
+    def stamp(self, G, _B, _C, _D, _E, _unknown_currents):
         G[self.n1, self.n1] += 1 / self.value
         G[self.n0, self.n0] += 1 / self.value
         G[self.n1, self.n0] -= 1 / self.value
@@ -73,7 +97,7 @@ class Inductor(Component):
     def __init__(self, n1, n0, value=0, name=None):
         super().__init__(n1, n0, value, name)
 
-    def stamp(self, G, _B, _C, _D, _I, _E, _unknown_currents):
+    def stamp(self, G, _B, _C, _D, _E, _unknown_currents):
         G[self.n1, self.n1] += self.value
         G[self.n0, self.n0] += self.value
         G[self.n1, self.n0] -= self.value
@@ -84,22 +108,23 @@ class OpenCircuit(Component):
     def __init__(self, n1, n0, value=0, name=None):
         super().__init__(n1, n0, value, name)
 
-    def stamp(self, _G, _B, _C, _D, _I, _E, _unknown_currents):
+    def stamp(self, _G, _B, _C, _D, _E, _unknown_currents):
         pass
 
+
 class ShortCircuit(Component):
-    def __init__(self, n1, n0, value=0, name=None)
+    def __init__(self, n1, n0, value=0, name=None):
         super().__init__(n1, n0, value, name)
         self.norator = Norator(n1, n0, value, name)
         self.nullator = Norator(n1, n0, value, name)
 
-    def stamp(self, _G, _B, _C, _D, _I, _E, _unknown_currents):
+    def stamp(self, _G, _B, _C, _D, _E, _unknown_currents):
         idx = num_unknown_currents(_unknown_currents)
         unknown_currents.append(self)
 
         # Combined nullator and norator
-        self.norator.stamp(_G, _B, _C, _D, _I, _E, _unknown_currents)
-        self.nullator.stamp(_G, _B, _C, _D, _I, _E, _unknown_currents)
+        self.norator.stamp(_G, _B, _C, _D, _E, _unknown_currents)
+        self.nullator.stamp(_G, _B, _C, _D, _E, _unknown_currents)
 
 
 class Nullator(Component):
@@ -109,27 +134,24 @@ class Nullator(Component):
     def num_unknown_currents(self):
         return 1
 
-    def stamp(self, _G, _B, _C, _D, _I, _E, _unknown_currents):
+    def stamp(self, _G, _B, _C, _D, _E, _unknown_currents):
         idx = num_unknown_currents(_unknown_currents)
         unknown_currents.append(self)
 
         C[idx, self.n1] += 1
         C[idx, self.n0] -= 1
 
+
 class Norator(Component):
     def __init__(self, n1, n0, value=0, name=None):
         super().__init__(n1, n0, value, name)
 
-    def stamp(self, _G, _B, _C, _D, _I, _E, _unknown_currents):
+    def stamp(self, _G, _B, _C, _D, _E, _unknown_currents):
         B[self.n1, idx] += 1
         B[self.n0, idx] -= 1
 
 
 class TwoPort(Component):
-    # + n1 --\   /-- + n3
-    #         ? ?
-    #         | |
-    # - n0 --/   \-- - n2
     def __init__(self, n1, n0, n3, n2, value=0, name=None):
         super().__init__(n1, n0, value, name)
         self.n3, self.n2 = n3, n2
@@ -137,15 +159,17 @@ class TwoPort(Component):
     def nodes(self):
         return self.n1, self.n0, self.n3, self.n2
 
+
 class VoltageControlledCurrentSource(TwoPort):
     def __init__(self, n1, n0, n3, n2, value=0, name=None):
         super().__init__(n1, n0, n3, n2, value, name)
 
-    def simulate(self, _G, _B, _C, _D, _I, _E, _unknown_currents):
-        G[self.n3, self.n3] += self.value
-        G[self.n2, self.n2] += self.value
-        G[self.n3, self.n2] -= self.value
-        G[self.n2, self.n3] -= self.value
+    def stamp(self, G, _B, _C, _D, _E, _unknown_currents):
+        G[self.n3, self.n1] -= self.value
+        G[self.n3, self.n0] += self.value
+        G[self.n2, self.n1] += self.value
+        G[self.n2, self.n0] -= self.value
+
 
 class VoltageControlledVoltageSource(TwoPort):
     def __init__(self, n1, n0, n3, n2, value=0, name=None):
@@ -154,8 +178,8 @@ class VoltageControlledVoltageSource(TwoPort):
     def num_unknown_currents(self):
         return 1
 
-    def simulate(self, _G, _B, _C, _D, _I, _E, _unknown_currents):
-        idx = num_unknown_currents(_unknown_currents)
+    def stamp(self, _G, B, C, _D, _E, unknown_currents):
+        idx = num_unknown_currents(unknown_currents)
         unknown_currents.append(self)
 
         # TODO: Test that this code is indeed correct wrt polarity
@@ -166,6 +190,7 @@ class VoltageControlledVoltageSource(TwoPort):
         C[idx, self.n1] -= self.value
         C[idx, self.n0] += self.value
 
+
 class CurrentControlledCurrentSource(TwoPort):
     def __init__(self, n1, n0, n3, n2, value=0, name=None):
         super().__init__(n1, n0, n3, n2, value, name)
@@ -173,8 +198,8 @@ class CurrentControlledCurrentSource(TwoPort):
     def num_unknown_currents(self):
         return 1
 
-    def simulate(self, _G, _B, _C, _D, _I, _E, _unknown_currents):
-        idx = num_unknown_currents(_unknown_currents)
+    def stamp(self, _G, B, C, _D, _E, unknown_currents):
+        idx = num_unknown_currents(unknown_currents)
         unknown_currents.append(self)
 
         # TODO: Test that this code is indeed correct wrt polarity
@@ -185,6 +210,7 @@ class CurrentControlledCurrentSource(TwoPort):
         C[idx, self.n1] += 1
         C[idx, self.n0] -= 1
 
+
 class CurrentControlledVoltageSource(TwoPort):
     def __init__(self, n1, n0, n3, n2, value=0, name=None):
         super().__init__(n1, n0, n3, n2, value, name)
@@ -192,21 +218,20 @@ class CurrentControlledVoltageSource(TwoPort):
     def num_unknown_currents(self):
         return 2
 
-    def simulate(self, _G, _B, _C, _D, _I, _E, _unknown_currents):
+    def stamp(self, _G, _B, _C, _D, _E, _unknown_currents):
         idx = num_unknown_currents(_unknown_currents)
         unknown_currents.append(self)
 
         # TODO: Test that this code is indeed correct wrt polarity
         B[self.n1, idx] += 1
         B[self.n0, idx] -= 1
-        B[self.n3, idx+1] += 1
-        B[self.n2, idx+1] -= 1
+        B[self.n3, idx + 1] += 1
+        B[self.n2, idx + 1] -= 1
         C[idx, self.n1] += 1
         C[idx, self.n0] -= 1
-        C[idx+1, self.n3] += 1
-        C[idx+1, self.n2] -= 1
-        D[idx+1, idx] -= self.value # TODO
-
+        C[idx + 1, self.n3] += 1
+        C[idx + 1, self.n2] -= 1
+        D[idx + 1, idx] -= self.value  # TODO
 
 
 class OperationalAmplifier(TwoPort):
@@ -216,14 +241,15 @@ class OperationalAmplifier(TwoPort):
     def num_unknown_currents(self):
         return 1
 
-    def simulate(self, _G, _B, _C, _D, _I, _E, _unknown_currents):
-        idx = num_unknown_currents(_unknown_currents)
+    def stamp(self, _G, B, C, _D, _E, unknown_currents):
+        idx = num_unknown_currents(unknown_currents)
         unknown_currents.append(self)
 
         B[self.n3, idx] += 1
         B[self.n2, idx] -= 1
         C[idx, self.n1] += 1
         C[idx, self.n0] -= 1
+
 
 class IdealTransformer(TwoPort):
     def __init__(self, n1, n0, n3, n2, l1=0, l2=0, name=None):
@@ -233,38 +259,48 @@ class IdealTransformer(TwoPort):
     def num_unknown_currents(self):
         return 2
 
-    def simulate(self, _G, _B, _C, _D, _I, _E, _unknown_currents):
+    def stamp(self, _G, _B, _C, _D, _E, _unknown_currents):
         assert False
         pass
 
 
 def num_nodes(components):
-    return len(set(node for component in components for node in component.nodes()))
+    return len(nodes(components))
+
+
+def nodes(circuit):
+    return set(node for component in circuit for node in component.nodes())
+
 
 def num_unknown_currents(components):
-    return sum(components.num_unknown_currents() for component in components)
+    return sum(component.num_unknown_currents() for component in components)
 
-def solve_mna(components):
+
+def solve_mna(components, zeros=np.zeros, block=np.block, return_populated_system=False):
     g_size = num_nodes(components)
-    b_size = sum(1 for c in components if isinstance(c, VoltageSource))
+    b_size = num_unknown_currents(components)
 
-    G = np.zeros((g_size, g_size))
-    B = np.zeros((g_size, b_size))
-    C = np.zeros((b_size, g_size))
-    D = np.zeros((b_size, b_size))
-    I = np.zeros(g_size + b_size)
-    E = np.zeros(g_size + b_size)
+    assert list(nodes(components)) == list(range(g_size)), "Nodes must be in a range from 0 (ground) to num_nodes - 1"
+
+    G = zeros((g_size, g_size))
+    B = zeros((g_size, b_size))
+    C = zeros((b_size, g_size))
+    D = zeros((b_size, b_size))
+    E = zeros(g_size + b_size)
     unknown_currents = []
 
     for component in components:
-        component.stamp(G, B, C, D, I, E, unknown_currents)
+        component.stamp(G, B, C, D, E, unknown_currents)
 
-    A = np.block([
+    A = block([
         [G, B],
         [C, D]
     ])
 
+    if return_populated_system:
+        return A[1:, 1:], E[1:]
+
     # NOTE: The number of independent KCLs is always one less than the number of nodes.
     # Any node may be chosen as *datum node* (ground node) and the solution will be the same.
     # I choose the ground node as the datum node, so I discard the first row and column of the matrix.
-    return np.linalg.solve(A[1:, 1:], E[1:] - I[1:])  # Discard ground node
+    return np.linalg.solve(A[1:, 1:], E[1:])  # Discard ground node
--- test_mna.py
+++ test_mna.py
@@ -1,5 +1,11 @@
-import numpy as np
-from mna import num_nodes, VoltageSource, CurrentSource, Resistor, solve_mna
+from mna import *
+
+import sympy as sym
+
+
+def sym_solve_mna(circuit):
+    return map(sym.Matrix, solve_mna(circuit, zeros=lambda x: sym.Matrix(np.zeros(x)), block=sym.BlockMatrix,
+                                     return_populated_system=True))
 
 
 def test_num_nodes():
@@ -21,7 +27,7 @@ def test_bassman_tonestack_resistors_only():
     ]
 
     assert num_nodes(bassman_tonestack_2) == 5
-    assert np.allclose(solve_mna(bassman_tonestack_2), np.array([1, 3/4, 3/4, 1/2, -1/2]))
+    assert np.allclose(solve_mna(bassman_tonestack_2), np.array([1, 3 / 4, 3 / 4, 1 / 2, -1 / 2]))
 
     bassman_tonestack_2 = [
         VoltageSource(1, 0, 1),
@@ -48,6 +54,35 @@ def test_simple_circuit():
     ]
     assert num_nodes(components) == 5
     assert np.allclose(solve_mna(components), np.array([1, 3 / 4, 3 / 4, 1 / 2, -1 / 2]))
+
+
+def test_simple_circuit_2():
+    r1, r2, r3, r4, r5, v1 = sym.symbols('r1, r2, r3, r4, r5, v1')
+    hd5_1 = [
+        VoltageSource(1, 0, v1),
+        Resistor(1, 2, r1),
+        Resistor(1, 3, r2),
+        Resistor(2, 3, r3),
+        Resistor(2, 0, r4),
+        Resistor(3, 0, r5),
+    ]
+
+    assert num_nodes(hd5_1) == 4
+    assert num_unknown_currents(hd5_1) == 1
+
+    g1, g2, g3, g4, g5 = 1 / r1, 1 / r2, 1 / r3, 1 / r4, 1 / r5
+    A_hand = sym.Matrix([
+        [g1 + g2, -g1, -g2, 1],
+        [-g1, g1 + g3 + g4, -g3, 0],
+        [-g2, -g3, g2 + g3 + g5, 0],
+        [1, 0, 0, 0]
+    ])
+    E_hand = sym.Matrix([0, 0, 0, v1])
+
+    A, E = sym_solve_mna(hd5_1)
+
+    assert A == A_hand
+    assert E == E_hand
 
 
 def test_circuit_resistor_voltage_source():
@@ -106,20 +141,209 @@ def test_current_source_resistor_galore():
 
     assert np.allclose(solve_mna(hd2_4), np.array(hd2_4_direct).astype(float).T)
 
-def test_dc_blocker_capacitor():
-    assert False
 
-def test_simple_inductor_circuit():
-    assert False
+def test_op_adder_circuit():
+    hd3_3 = [
+        ResistiveVoltageSource(1, 0, 1, 1),
+        ResistiveVoltageSource(2, 0, 1, 1),
+        ResistiveVoltageSource(2, 0, 1, 1),
+        Resistor(1, 3, 1),
+        OperationalAmplifier(2, 1, 3, 0)
+    ]
+    g1 = g2 = g3 = g4 = v1 = v2 = v3 = 1
+    hd3_3_direct = np.linalg.solve(np.array([
+        [g4, 0, -g4, 1, 0, 0, 0],
+        [0, 0, 0, 0, 1, 1, 0],
+        [-g4, 0, g4, 0, 0, 0, 1],
+        [1, 0, 0, -1 / g3, 0, 0, 0],
+        [0, 1, 0, 0, -1 / g1, 0, 0],
+        [0, 1, 0, 0, 0, -1 / g2, 0],
+        [-1, 1, 0, 0, 0, 0, 0]
+    ]), np.array([0, 0, 0, v1, v2, v3, 0]))
+    print(solve_mna(hd3_3), np.array(hd3_3_direct).astype(float).T)
+    assert num_nodes(hd3_3) == 4
+    assert np.allclose(solve_mna(hd3_3), np.array(hd3_3_direct).astype(float).T)
+
+
+def test_resistive_voltage_source():
+    volt_source = [
+        VoltageSource(1, 0, 1),
+        Resistor(1, 2, 1),
+        Resistor(2, 3, 1),
+    ]
+    resistive_volt_source = [
+        ResistiveVoltageSource(1, 0, 1, 1),
+        Resistor(1, 2, 1),
+    ]
+    assert np.allclose(solve_mna(volt_source)[1:], solve_mna(resistive_volt_source))
+
 
 def test_vcvs():
-    assert False
+    r1, r2, r3, mu, i1 = sym.symbols('r1, r2, r3, mu, i1')
+    hd3_4 = [
+        CurrentSource(1, 0, i1),
+        Resistor(1, 0, r1),
+        Resistor(1, 2, r2),
+        Resistor(2, 0, r3),
+        VoltageControlledVoltageSource(1, 0, 2, 1, mu)
+    ]
+
+    assert num_nodes(hd3_4) == 3
+    assert num_unknown_currents(hd3_4) == 1
+
+    A_hand = sym.Matrix([
+        [1 / r1 + 1 / r2, -1 / r2, -1],
+        [-1 / r2, 1 / r2 + 1 / r3, 1],
+        [-1 - mu, 1, 0]
+    ])
+    E_hand = sym.Matrix([i1, 0, 0])
+
+    A, E = sym_solve_mna(hd3_4)
+
+    assert A == A_hand
+    assert E == E_hand
+
 
 def test_ccvs():
-    assert False
+    pass
+
+
+def test_cccs():
+    r1, r2, r3, r4, mu, i1, v1 = sym.symbols('r1, r2, r3, r4, mu, i1, v1')
+    hd5_2 = [
+        CurrentControlledCurrentSource(1, 4, 1, 0, mu),
+        VoltageSource(1, 4, v1),
+        CurrentSource(2, 0, i1),
+        Resistor(1, 2, r1),
+        Resistor(4, 3, r2),
+        Resistor(2, 3, r3),
+        Resistor(3, 0, r4)
+    ]
+
+    assert num_nodes(hd5_2) == 5
+    assert num_unknown_currents(hd5_2) == 2
+
+    g1, g2, g3, g4 = 1 / r1, 1 / r2, 1 / r3, 1 / r4
+    A_hand = sym.Matrix([
+        [g1, -g1, 0, 0, -mu+1],
+        [-g1, g1+g3, -g3, 0, 0],
+        [0, -g3, g2+g3+g4, -g2, 0],
+        [0, 0, -g2, g2, -1],
+        [1, 0, 0, -1, 0],
+    ])
+    E_hand = sym.Matrix([0, i1, 0, 0, v1])
+
+    A, E = sym_solve_mna(hd5_2)
+
+    # assert A == A_hand
+    assert E == E_hand
+
+
+def test_non_planar_circuit():
+    r1, r2, r3, r4, v1, i1 = sym.symbols('r1, r2, r3, r4, v1, i1')
+    circuit = [
+        Resistor(1, 0, r1),
+        ResistiveVoltageSource(1, 0, volt=v1, resistance=r3),
+        Resistor(1, 2, r2),
+        Resistor(2, 0, r4),
+        CurrentSource(0, 2, i1)
+    ]
+
+    assert num_nodes(circuit) == 3
+    assert num_unknown_currents(circuit) == 1
+
+    A_hand = sym.Matrix([
+        [1 / r1 + 1 / r2, -1 / r2, 1],
+        [-1 / r2, 1 / r2 + 1 / r4, 0],
+        [1, 0, -r3]
+    ])
+    E_hand = sym.Matrix([0, -i1, v1])
+
+    A, E = sym_solve_mna(circuit)
+
+    assert A == A_hand
+    assert E == E_hand
+
 
 def test_vccs():
-    assert False
+    # hd4_1
+    r1, r2, r3, r4, mu = sym.symbols('r1 r2 r3 r4 mu')
+    sym_hd4_1 = [
+        Resistor(1, 0, r1 + r2),
+        Resistor(1, 0, r3),
+        Resistor(1, 2, r4),
+        VoltageControlledCurrentSource(1, 0, 2, 0, mu),
+        CurrentSource(2, 0, 1)
+    ]
+
+    assert num_nodes(sym_hd4_1) == 3
+    assert num_unknown_currents(sym_hd4_1) == 0
+
+    A_hand = sym.Matrix([
+        [1 / (r1 + r2) + 1 / r3 + 1 / r4, -1 / r4],
+        [-1 / r4 - mu, 1 / r4]
+    ])
+    E_hand = sym.Matrix([0, 1])
+
+    A, E = sym_solve_mna(sym_hd4_1)
+
+    assert A_hand == A
+    assert E_hand == E
+
+    r1, r2, r3, r4, mu = 30, 20, 4, 5, 2
+    hd4_1_matrix = np.linalg.solve(np.array([
+        [1 / (r1 + r2) + 1 / r3 + 1 / r4, -1 / r4],
+        [-1 / r4 - mu, 1 / r4]
+    ]), np.array([0, 1]))
+
+    solved = A.LUsolve(E).subs({'r1': r1, 'r2': r2, 'r3': r3, 'r4': r4, 'mu': mu})
+    assert np.allclose(np.array(solved).astype(float).T, hd4_1_matrix)
+
 
 def test_op_amp():
-    assert False
+    # hd3_1
+    v_in = 42
+    circuit = [
+        VoltageSource(1, 0, v_in),
+        OperationalAmplifier(1, 2, 3, 0),
+        Resistor(2, 0, 10e3),
+        Resistor(2, 3, 10e3),
+        Resistor(3, 4, 2e3),
+        Resistor(4, 0, 8e3),
+        Resistor(5, 0, 10e3),
+        OperationalAmplifier(4, 5, 6, 0),
+        Resistor(5, 6, 10e3),
+    ]
+
+    assert num_nodes(circuit) == 7
+    assert np.allclose(solve_mna(circuit), v_in * np.array([1.0, 1.0, 2.0, 1.6, 1.6, 16 / 5, 0.0, - 3.0e-04, -1.6e-04]))
+
+
+def test_op_amp2():
+    # hd3_2
+    v_in = 5
+    circuit = [
+        VoltageSource(1, 0, v_in),
+        Resistor(1, 2, 1),
+        Resistor(2, 4, 1),
+        Resistor(2, 3, 2),
+        Resistor(3, 4, 3),
+        Resistor(4, 5, 2),
+        Resistor(5, 0, 1),
+        OperationalAmplifier(5, 3, 4, 0),
+    ]
+
+    g1, g2, g3, g4, g5, g6 = 1 / 1, 1 / 1, 1 / 2, 1 / 3, 1 / 2, 1 / 1
+
+    hd3_2 = np.linalg.solve(np.array([
+        [g1, -g1, 0, 0, 0, 1, 0],
+        [-g1, g1 + g2 + g3, -g3, -g2, 0, 0, 0],
+        [0, -g3, g3 + g4, -g4, 0, 0, 0],
+        [0, -g2, -g4, g2 + g4 + g5, -g5, 0, 1],
+        [0, 0, 0, -g5, g5 + g6, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0],
+        [0, 0, -1, 0, 1, 0, 0],
+    ]), np.array([0, 0, 0, 0, 0, v_in, 0]))
+
+    assert num_nodes(circuit) == 6
+    assert np.allclose(solve_mna(circuit), np.array(hd3_2).astype(float).T)
